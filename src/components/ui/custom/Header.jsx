bdd09be240c9953c2624411cab7a552ad1cd7593
sumitdebnath2002 <sumitdebnath.1920@gmail.com>
2025-07-06 22:02:58 +0530

    fixed logout problem

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -83,7 +83,7 @@ function Header() {
                   onClick={() => {
                     googleLogout();
                     localStorage.clear();
-                    window.location.reload();
+                    window.location.href = "/";
                   }}
                 >
                   Logout
